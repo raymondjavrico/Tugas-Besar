768b9d8a11e182422039c356d0afbe382252752c
indiraamada <89306121+indiraamada@users.noreply.github.com>
2021-10-26 09:38:51 +0700

    Update tubes_11.py

--- tubes_11.py
+++ tubes_11.py
@@ -7,167 +7,127 @@
 # cons: bikin flowchart & pseudocodenya ribet
 
 from random import randint
+
+#Bacanya dari paling bawah
+
+logged=False
+app = True
 arr_name = ["empty"]
 arr_email = ["empty"]
 arr_phone = [0]
 arr_password = ["empty"]
-attempt = 0
-repeat = False
-ngulang = False
-gewees = False
-result = True
-satu = True
-daiya = False
 welcome = "Welcome to TempDec!"
-name = ""
-valid = ""
 
-while result:
-    for e in range(len(welcome)+2):
-        print("=", end="")
-    print()
-    print("", welcome)
-    for i in range(len(welcome)+2):
-        print("=", end="")
-    acc = input("\nAlready have an account? log in to continue! (y/n): ")
-    if acc == "y" or acc == "Y":
-        daiya = True
-    elif acc == "n" or acc == "N":
-        dar = input("Please sign in to use the application! (click ok to continue): ").lower()
-        if dar == "ok":
-            m = input("TempDec wants to ask for access to your location, files, and camera (click ok to continue): ").lower()
-            if m == "ok":
-                while satu:
-                    email = input("Please enter your email         : ")
-                    daiya = True
-                    for element in arr_email:
-                        if element == email:
-                            print("This email address has already been registered into another account. Please choose another one.")
-                            ngulang = True
-                            daiya = False
-                    if daiya:
-                        arr_email.append(email)
-                        satu = False
-                        daiya = False
-                while not satu:
-                    phone = input("Please enter your phone number  : ")
-                    daiya = True
-                    for element in arr_phone:
-                        if element == phone:
-                            print("This phone number has already been registered into another account. Please choose another one.")
-                            ngulang = True
-                            daiya = False
-                    if daiya:
-                        arr_phone.append(phone)
-                        satu = True
-                        daiya = False
-                while satu:
-                    name = input("Please enter your username      : ")
-                    daiya = True
-                    for element in arr_name:
-                        if element == name:
-                            print("This username has already been taken. Please choose another one.")
-                            ngulang = True
-                            daiya = False
-                    if daiya:
-                        arr_name.append(name)
-                        satu = False
-                        daiya = False
-                password = input("Please enter your password      : ")
-                arr_password.append(password)
-                print("Hello, " + name + "!" + " we have sent a code to your device, please insert the code to continue! ")
-                repeat = True
-            else:
-                print("Please click ok to continue.")
-                ngulang = False
-                daiya = False
-        else:
-            print("Please click ok to continue.")
-            ngulang = False
-            daiya = False
-    else:
-        print("Invalid input. Please enter either y/n.\n")
+def regis_email():
+    loop =True
+    global arr_email
+    while loop:
+        email = input("Please enter your email          : ")
+        confirm=True
+        for element in arr_email:
+            if element == email:
+                confirm = False
+                print("This email address has already been registered into another account. Please choose another one.")
+        if confirm:
+            arr_email.append(email)
+            loop = False
+    return 
+            
+def regis_phone():
+    loop =True
+    global arr_phone
+    while loop:
+        phone = input("Please enter your phone number   : ")
+        confirm=True
+        for element in arr_phone:
+            if element == phone:
+                confirm = False
+                print("This phone number has already been registered into another account. Please choose another one.")
+        if confirm:
+            arr_phone.append(phone)
+            loop = False
+    return 
 
-    while daiya:
-        gewees = True
-        name = input("Please enter your username      : ")
-        password = input("Please enter your password      : ")
-        for e in arr_name:
-            if e == name:
-                valid = "Valid Username! Welcome, " + name
-                ngulang = True
-        for m in arr_password:
-            if m == password and ngulang:
-                print(valid)
-                gewees = False
-                daiya = False
-        if gewees:
-            print("Invalid Password or Username! Try another one! ")
-            attempt += 1
-            if attempt == 3:
-                print("You lose attempts, please try again later.\n")
-                attempt = 0
-                gewees = False
-                ngulang = False
-                daiya = False
+def regis_username():
+    loop = True
+    global arr_name, arr_password
+    while loop:
+        username = input("Please enter your username     : ")
+        confirm=True
+        for element in arr_name:
+            if element == username:
+                confirm = False
+                print("This username has already been taken. Please choose another one.")
+        if confirm:
+            arr_name.append(username)
+            loop = False
+    password = input("Please enter your password      : ")
+    arr_password.append(password)
+    return 
 
-    arr = []
-    while repeat:
-        jia = randint(100, 1000)
-        repeat = ("y" or "yes") in input("You received the code: " + str(jia) + ", do you want us to send another code? (y/n): ").lower()
-        arr.append(jia)
-        daiya = True
-
-    while daiya:
+def regis_otp():
+    arr_otp = []
+    confirm=True
+    loop=True
+    while loop:
+        otp = randint(100, 1000)
+        loop = ("y" or "yes") in input("You received the code: " + str(otp) + ", do you want us to send another code? (y/n): ").lower()
+        arr_otp.append(otp)
+    loop=True
+    while loop:
         code = int(input("Insert the code you have received: "))
-        if code == arr[len(arr)-1]:
-            daiya = False
-            memo = input("I accept the privacy and policy terms of this application: ")
-            if memo == "y" or memo == "yes":
-                print("Congratulations! You have succesfully signed in to TempDec ^_^")
-                ngulang = True
-                daiya = False
-
-            elif memo == ("n" or "no") in input().lower():
-                result = False
+        if code == arr_otp[len(arr_otp)-1]:
+            memo = input("I accept the privacy and policy terms of this application (y/n): ")
+            while confirm:
+                if memo == "y" or "yes" or "oke" or "ok":
+                    print("Congratulations! You have succesfully signed in to TempDec ^_^")
+                    loop=False
+                    confirm=False
+                else:
+                    print("You must agree to the privacy and policy terms of this application to continue using this app.")
         else:
             print("Please enter the latest code.")
 
-# bagian 2 (dalam aplikasi)
+#########################################################################################################################################
+def regis_main(logged):
+    reg = input("Please register in to use the application! (click ok to continue): ").lower()
+    if reg == "ok":
+        m = input("TempDec wants to ask for access to your location, files, and camera (click ok to continue): ").lower()
+        if m == "ok":
+            regis_email() 
+            regis_phone()
+            regis_username()
+            regis_otp()
+            logged=True
+            return logged
+#########################################################################################################################################
+def login_main(logged):
+    global arr_name, arr_password
+    loop = True
+    n = 5
+    while loop:
+        name = input("Please enter your username       :")
+        password = input("Please enter your password    :")
+        i=-1
+        for e in arr_name:
+            i+=1
+            if name == e:
+                if password == arr_password[i]:
+                    loop = False
+                    logged = True
+                    return logged
+        n-=1
+        print(f"Invalid Password or Username! Try another one. ({n} more attempt)")
+        if n==0:
+            print("You lose attempts, please try again later.\n")
+            return logged
+#########################################################################################################################################
 
-    arrey = ["Info Penting", "Temperature Detector", "Scan QR Code", "Diary Perjalanan", "Pendaftaran Vaksin"]
-    if ngulang:
-        print()
-        items = "list of available functions "
-        for i in range(len(items)+2):
-            print("=", end="")
-        print()
-        print("", items)
-        for i in range(len(items)+2):
-            print("=", end="")
-            gewees = True
-    while gewees:
-        print()
-        for e in range(len(arrey)):
-            print(str(e+1) + ". " + arrey[e])
-        B = int(input("\nChoose one of the above: "))
-        if B == 1:
-            info = "\nBla bla bla fafifu was wes wos pokoknya ini informasi penting banget jadi tempdec ini itu \n" \
-                "seperti semacam aplikasi dimana kamu bisa .... dan bisa ngecek temperatur juga tapi di sini cuma \n" \
-                "kurang lebih simulasi doang ga ngecek beneran, terus bisa scan QR code juga untuk mengetahui apakah \n" \
-                "kita bisa masuk ke suatu tempat atau tidak. Ada juga diary perjalanan untuk mengetahui tempat \n" \
-                "apa saja yang sudah kita kunjungi, dan terdapat juga pendaftaran vaksin."
-            print(info)
-            back = input("\nDo you want to go back? (y/n): ")
-            if back == "y" or back == "Y":
-                gewees = True
-            else:
-                print("Thanks for using TempDec ^_^\n")
-                attempt = 0
-                satu = True
-                gewees = False
-    # Bagian B = 2-5 kalian isi langsung sesuai pembagian tugasnya ya gais, jadi
-    # 2 = Temperature Detector
+def menu_1():
+    print("Info Penting")
+
+def menu_2():
+    print("Temperature Detector")
     arr = []
     while result:
         suhu = int(input("Masukkan suhu: "))
@@ -182,17 +142,74 @@ while result:
                 rata = sum(arr)/(len(arr))
                 result = "y" in input("Rata-rata suhu anda:" + str(rata) + " input suhu lagi? (y/n): ").lower()
             result != "n"
-    # 3 = Scan QR Code
-    # 4 = Diary Perjalanan
-    # 5 = Pendaftaran Vaksin
-        elif B == 2:
-            print("Temperature Detector")
-        elif B == 3:
-            print("QR Code")
-        elif B == 4:
-            print("Diary Perjalanan")
-            print("Belum ada data terkait riwayat lokasi perjalanan Anda")
-        elif B == 5:
-            print("Pendaftaran Vaksin")
-        else:
-            print("invalid input. Please enter only 1-5.")
+def menu_3():
+    print("QR Code")
+
+def menu_4():
+    print("Diary Perjalanan")
+
+def menu_5():
+    print("Pendaftaran Vaksin")
+
+def menu_invalid():
+    print("invalid input. Please enter only 1-5")   
+
+#########################################################################################################################################
+def main_menu(logged):
+    arr_menu = ["Info Penting", "Temperature Detector", "Scan QR Code", "Diary Perjalanan", "Pendaftaran Vaksin"]
+    print()
+    items = "list of available functions "
+    for e in range(len(items)+2):
+        print("=", end="")
+    print()
+    print("", items)
+    for e in range(len(items)+2):
+        print("=", end="")
+    print()
+    for e in range(len(arr_menu)):
+        print(str(e+1) + ". " + arr_menu[e])
+    print()
+    print()
+    y = input("Tekan Q Jika ingin keluar")
+    if y == "Q" or "q":
+        z = input("Tekan Q sekali lagi jika ingin keluar")
+        if z == "Q" or "q":
+            logged = False
+            print("Thanks for using TempDec ^_^\n")
+            return logged
+    
+    B = int(input("\nChoose one of the above: "))
+    if B == 1:
+        menu_1()
+    elif B == 2:
+        menu_2()
+    elif B == 3:
+        menu_3()
+    elif B == 4:
+        menu_4()
+    elif B == 5:
+        menu_5()
+    else:
+        menu_invalid()
+
+#########################################################################################################################################
+
+while app:
+    for e in range(len(welcome)+2):
+        print("=", end="")
+    print()
+    print("", welcome)
+    for i in range(len(welcome)+2):
+        print("=", end="")
+    acc = input("\nAlready have an account? log in to continue! (y/n): ")
+    if acc == "y" or acc == "Y":
+        logged = login_main(logged)
+    elif acc == "n" or acc == "N":
+        logged = regis_main(logged)
+    while logged:
+        logged = main_menu(logged)
+    y = input("Exit? (y/n)  :")
+    if y == "y":
+        exit()
+
+#########################################################################################################################################
